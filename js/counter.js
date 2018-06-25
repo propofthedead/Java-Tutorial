9d5318db29a48dc07146a0fcaaf0f677730124a7
Foster Stule <camodude5@yahoo.com>
2018-06-25 12:35:12 -0400

    it works

--- js/counter.js
+++ js/counter.js
@@ -2,13 +2,15 @@
 var theNumber=0;
 
 var inputCtrl=document.getElementById("value");
-
+inputCtrl.value=theNumber;
 function add(){
+    var inputCtrl=document.getElementById("value");
     theNumber++;
     inputCtrl.value= theNumber;
 }
 
 function sub(){
+    var inputCtrl=document.getElementById("value");
     theNumber--;
     inputCtrl.value=theNumber;
 }
