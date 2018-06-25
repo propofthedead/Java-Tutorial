5b346ee239bb753e0c782b1eb50a87c15a393ad7
Foster Stule <camodude5@yahoo.com>
2018-06-25 14:25:57 -0400

    made it display different for special occasions"

--- js/counter.js
+++ js/counter.js
@@ -1,16 +1,48 @@
 "option strict"
 var theNumber=0;
+var inputCtrl=null;
 
-var inputCtrl=document.getElementById("value");
-inputCtrl.value=theNumber;
 function add(){
     var inputCtrl=document.getElementById("value");
     theNumber++;
     inputCtrl.value= theNumber;
+    display()
 }
 
 function sub(){
     var inputCtrl=document.getElementById("value");
     theNumber--;
     inputCtrl.value=theNumber;
+    display();
+}
+function loaded(){
+    inputCtrl=document.getElementById("value");
+    inputCtrl.value=theNumber;
+    
+}
+function display(){
+    inputCtrl.value=theNumber;
+    var isEven;
+    var div3;
+    if(theNumber %2 ==0)
+    {
+        isEven=true;
+    }else{
+        isEven=false;
+    }
+    if(isEven){
+        inputCtrl.style.color="red";
+    }else{
+        inputCtrl.style.color="black";
+    }
+    if(theNumber %3==0){
+        div3=true;
+    }else{
+        div3=false;
+    }
+    if(div3){
+        inputCtrl.style.fontWeight="bold";
+    }else{
+        inputCtrl.style.fontWeight="normal";
+    }
 }
