60d2bc82133ad5e4e25503e420dac2b14cd8793d
Foster Stule <camodude5@yahoo.com>
2018-06-25 11:55:52 -0400

    tested with some button clicks

---
+++ button-click.js
@@ -0,0 +1,19 @@
+"option strict"
+console.log("wired properly");
+
+function clicked(){
+    var inputCtrl =document.getElementById("thetext");
+    var inputValue= inputCtrl.value;
+    var labelCtrl= document.getElementById("theLabel");
+    labelCtrl.textContent= inputValue;
+
+}
+function add()
+{
+    var op1Ctrl= document.getElementById("op1");
+    var op2Ctrl= document.getElementById("op2");
+    var op1value= op1Ctrl.value;
+    var op2value= op2Ctrl.value;
+    var labelCtrl= document.getElementById("answer");
+    labelCtrl.textContent= Number(op1value)+Number(op2value);
+}
