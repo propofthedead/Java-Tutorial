89eff789224ca18fc6a2eaef01a251e24355f5eb
Foster Stule <camodude5@yahoo.com>
2018-06-26 09:53:49 -0400

    can call data made display fuction

--- js/student.js
+++ js/student.js
@@ -1,3 +1,31 @@
-"option explicit"
+"option strict"
 
-var students=[];
+var students=[
+    {id:0,name:"test",sat:1000,gpa:3.5,year:"FR"}
+];
+
+function loaded(){
+   display();
+}
+
+function add(){
+
+}
+
+function display(){
+    var tbody= document.getElementById("tbody");
+    tbody.innerHTML="";
+    for(var student of students){
+        var row="";
+        row+="<tr>";
+        row+="<td>"+student.id+"</td>";
+        row+="<td>"+student.name+"</td>";
+        row+="<td>"+student.gpa+"</td>";
+        row+="<td>"+student.sat+"</td>";
+        row+="<td>"+student.year+"</td>";
+        row+="</tr>";
+        tbody.innerHTML+=row;
+        
+
+    }
+}
