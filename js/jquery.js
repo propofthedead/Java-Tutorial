c6fb3514b93ed7224f3569a0999c7c46f717ceed
Foster Stule <camodude5@yahoo.com>
2018-06-27 13:36:47 -0400

    added jequery fuctions and corrected file location

--- js/jquery.js
+++ js/jquery.js
@@ -6,5 +6,11 @@ $(document).ready(function(){
     .val("This is default text in the input box")
     .css("color","red")
     .css("font-weight","bold");
+
+
+    $("button").click(function(){
+        var txt= $("#inpbox").val();
+        $("label").text(txt);
+    })
 });
 
