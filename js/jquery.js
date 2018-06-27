c07bb421348652017ce8c5ebad219eecaee8ce4e
Foster Stule <camodude5@yahoo.com>
2018-06-27 12:26:51 -0400

    diving into styling with jquery syntax

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+"option strict"
+
+
+$(document).ready(function(){
+    $("#inpbox")
+    .val("This is default text in the input box")
+    .css("color","red")
+    .css("font-weight","bold");
+});
+
