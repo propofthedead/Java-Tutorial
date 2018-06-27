94b292fbc11546eed365ec341fba452745606472
Foster Stule <camodude5@yahoo.com>
2018-06-27 10:02:53 -0400

    perfected the create method

--- js/AjaxCreate.js
+++ js/AjaxCreate.js
@@ -1,10 +1,15 @@
 "option strict"
 
 function create(){
-    var id= document.getElementById("iId").value;
-    $.getJSON("http://localhost:63558/Customers/Get/"+id)
+    var customer={
+        Id:0,
+        Name: document.getElementById("pName").value,
+        State: document.getElementById("pState").value,
+        City: document.getElementById("pCity").value,
+        Preferred: document.getElementById("pPreffered").checked
+    }
+    $.post("http://localhost:63558/Customers/Create",customer)
     .done(function(resp){
         console.log(resp);
-        display(resp);
     });
 }
